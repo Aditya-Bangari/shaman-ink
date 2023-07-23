a323d030f7f2df07073cbec7ff6c408d9be90b36
AdityaaBangari <aditya.bangari@symbtechnologies.com>
2023-07-23 23:57:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreloaderComponent } from './preloader/preloader.component';
+import { HomeComponent } from './home/home.component';
+import { HomeRoutingModule } from './home/home-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreloaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
