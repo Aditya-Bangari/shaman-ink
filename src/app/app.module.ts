00f5f882103b63debf52de5497bd8ef4f7a31638
AdityaaBangari <aditya.bangari@symbtechnologies.com>
2023-10-16 01:06:53 +0530

    maain html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PreloaderComponent } from './preloader/preloader.component';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home/home-routing.module';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreloaderComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import LocomotiveScroll from 'locomotive-scroll';
 import { gsap } from 'gsap'
+declare var $: any; // Import jQuery
 import SplitType from 'split-type'
 @Component({
   selector: 'app-home',
@@ -8,18 +9,60 @@ import SplitType from 'split-type'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit  {
+  showPreLoader = false;
   constructor() {
   }
   ngOnInit() {
-    this.call();
-  }
-  call() {
-    const scroll = new LocomotiveScroll({
-      el: document.querySelector('[data-scroll-container]') as HTMLElement,
-      smooth: true,
-      repeat: true,
-      multiplier: 0.9
+
+    // Handle left arrow click
+    $(".arrowhead-left").click(function () {
+      $("#myCarousel").carousel("prev");
     });
+
+    // Handle right arrow click
+    $(".arrowhead-right").click(function () {
+      $("#myCarousel").carousel("next");
+    });
+
+    const images = document.querySelectorAll('img');
+    let imagesLoaded = 0;
+
+    images.forEach((image) => {
+      if (image.complete) {
+        imagesLoaded++;
+        this.preLoaderTimeout();
+      } else {
+        image.addEventListener('load', () => {
+          imagesLoaded++;
+          if (imagesLoaded === images.length) {
+            this.preLoaderTimeout();
+          }
+        });
+      }
+    });
+
+    // If all images are already loaded, initialize immediately
+    if (imagesLoaded === images.length) {
+      this.preLoaderTimeout();
+    }
+  }
+
+
+  preLoaderTimeout() {
+    setTimeout(() => {
+      this.showPreLoader = false; // Hide the preloader after the delay
+      this.call();
+    }, 5400); // Adjust the delay time as needed
+  }
+
+
+  call() {
+    // const scroll = new LocomotiveScroll({
+    //   el: document.querySelector('[data-scroll-container]') as HTMLElement,
+    //   smooth: true,
+    //   repeat: true,
+    //   multiplier: 0.9
+    // });
 
     const hehe = document.querySelectorAll('[title]');
     console.log(hehe);
